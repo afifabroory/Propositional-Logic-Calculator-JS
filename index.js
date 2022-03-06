ffb2e0e7f1d2ab143320921e4f5f15e20fe31878
afifabroory <afifabroory@gmail.com>
2022-03-06 19:54:11 +0800

    Working input output component

--- index.js
+++ index.js
@@ -1,30 +1,3 @@
-// ReactDOM.render(<h1>Hello, World!</h1>, document.querySelector("#root"))
-// var header = document.createElement("h1")
-// header.textContent = 'Hello, World!'
-// header.className = 'header'
-// document.querySelector("#root").appendChild(header);
-
-// function Navbar() {
-//     return (<ul>
-//         <li><a href="google.com">Home</a></li>
-//         <li><a href="google.com">News</a></li>
-//         <li><a href="google.com">Contact</a></li>
-//         <li><a href="google.com">About</a></li>
-//     </ul>)
-// }
-
-// function MainContent() {
-//     return <h1>Russia Won War!</h1>
-// }
-
-// ReactDOM.render(
-//     <div>
-//         <Navbar />
-//         <MainContent />
-//     </div>, 
-//     document.querySelector('#root')
-// );
-
 const {
     Container,
     InputBase,
@@ -33,8 +6,13 @@ const {
     Icon,
     createTheme,
     ThemeProvider,
-    Collapse
-  } = MaterialUI;
+    Collapse,
+    Table,
+    TableHead,
+    TableBody,
+    TableRow,
+    TableCell
+} = MaterialUI;
 
 const InputTheme = createTheme({
     components: {
@@ -53,66 +31,125 @@ const InputTheme = createTheme({
 
 function App() {
     const [checked, setChecked] = React.useState(false);
-    const [expr, setExpr] = React.useState('');
+    const [isOK, areUOK] = React.useState(false);
+    const [vars, setVar] = React.useState([]);
+
+    let isNotError = false;
+
+    const table_output = () => {
+        let header_column = [...vars.keys()];
+        let expr_value = document.getElementById('main').value.toUpperCase();
+
+        const BodyTable = () => {
+            const Readable = (v) => {
+                return v ? "T" : "F";
+            }
+
+            const Row = () => {
+                let id = 0;
+                return _var_row.map((e, i) => {
+                    let col = [...e.values()].map(e2 => {
+                        return <TableCell key={id++} align="center">{Readable(e2)}</TableCell>
+                    });
+                    col = col.concat(<TableCell key={id++} align="center">{Readable(_result_row[i])}</TableCell>);
+                    return <TableRow key={id++}>{col}</TableRow>
+                    
+                })
+            }
+
+            return (<TableBody>
+                <Row />
+            </TableBody>)
+        }
+
+        header_column.push(expr_value);
+        return (
+            <Table size="small">
+                <TableHead>
+                    <TableRow>
+                        {header_column.map(e => <TableCell key={e} align="center">{e}</TableCell>)}
+                    </TableRow>
+                </TableHead>
+                <BodyTable />
+            </Table>
+        );
+    }
 
     const handleResultOnClick = () => {
-        setChecked(false);
-        eval_expr(expr);
-        setTimeout(() => setChecked(true), 800);
+        try {
+            setChecked(true);
+            let expr_value = document.getElementById('main').value;
+            eval_expr(expr_value);
+            setVar(_vars_context);
+            areUOK(true);
+            setChecked(false);
+        } catch (err) {
+            areUOK(false);
+        }
     };
 
     const handleResultOnEnter = (e) => {
         if (e.key === 'Enter') {
-            setChecked(false);
-            eval_expr(expr);
-            setTimeout(() => setChecked(true), 800);
+            try {
+                let expr_value = document.getElementById('main').value;
+                setChecked(true);
+                eval_expr(expr_value);
+                setVar(_vars_context);
+                areUOK(true);
+            } catch (err) {
+                areUOK(false);
+            };
         }
     };
 
-    const handleChange = (e) => {
-        setExpr(e.target.value);
+    function Output() {
+        if (!isOK) return (<h3 style={{
+            textAlign: 'center'
+        }}>Sorry, we cannot understand your input. Please check again.</h3>);
+        else return table_output();
     }
 
+    const Main = (
+        <ThemeProvider theme={InputTheme}>
+            <Paper elevation={0} autoComplete="off" sx={{
+                border: 0.1,
+                borderColor: '#B6B6B6',
+                p: '6px 0px',
+                width: '100%',
+                maxWidth: 650,
+                display: 'flex'
+            }}>
+                <InputBase autoFocus fullWidth id="main" placeholder="Enter An Expression" onKeyDown={handleResultOnEnter} sx={{ ml: 2 }} />
+                <IconButton color="primary" size="small" onClick={handleResultOnClick} sx={{ mr: 1 }}>
+                    <Icon>calculaterounded</Icon>
+                </IconButton>
+            </Paper>
+        </ThemeProvider>
+    );
+
     return (
-        <Container sx={{ 
-            height: '100%', 
-            borderRadius:2,
+        <Container sx={{
+            height: '100%',
+            borderRadius: 2,
             display: 'flex',
             alignItems: 'center',
             justifyContent: 'center',
             flexDirection: 'column'
-            }} >
-          <ThemeProvider theme={InputTheme}>
-              <Paper elevation={0} sx={{
-                  border: 0.1,
-                  borderColor: '#B6B6B6',
-                  p: '6px 0px',
-                  width: '100%',
-                  maxWidth: 650,
-                  display: 'flex'
-              }}>
-                  <InputBase autoFocus fullWidth placeholder="Enter An Expression" onKeyDown={handleResultOnEnter} onChange={handleChange} sx={{ ml: 2 }} />
-                  <IconButton color="primary" size="small" onClick={handleResultOnClick} sx={{ mr: 1 }}>
-                      <Icon>calculaterounded</Icon>
-                  </IconButton>
-              </Paper>
-          </ThemeProvider>
-      
-          <Collapse in={checked} sx={{
-            width: '100%',
-            maxWidth: 650,
-            p: '6px 0px',
-            mt: 2
-          }}>
-              <Paper elevation={3} sx={{
-                  border: 0.1,
-                  borderColor: '#B6B6B6'
-              }}>
-                  <h3 style={{
-                      textAlign: 'center'
-                  }}>This is h3 inside parent Paper and ancestor Collapse</h3>
-              </Paper>
-          </Collapse>
+        }} >
+            {Main}
+            <Collapse in={checked} sx={{
+                width: '100%',
+                maxWidth: 650,
+                p: '6px 0px',
+                mt: 2
+            }}>
+                <Paper elevation={3} sx={{
+                    border: 0.1,
+                    borderColor: '#B6B6B6'
+                }}>
+                    <Output />
+                </Paper>
+            </Collapse>
         </Container>
     );
 }
